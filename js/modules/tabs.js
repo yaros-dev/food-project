35b849ebf405517350b525f108bdc7d90d0c5343
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2022-10-15 20:09:18 +0300

    divided into js modules

---
+++ js/modules/tabs.js
@@ -0,0 +1,43 @@
+function tabs() {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active')
+        });
+
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener("click", (e) => { // e - об'єкт події
+        const target = e.target;
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((element, i) => {
+                if (target == element) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+}
+
+
+module.exports = tabs;
