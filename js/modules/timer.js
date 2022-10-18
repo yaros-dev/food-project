fac562620dac072d4ada3918edb52c92fb0f2477
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2022-10-18 15:54:56 +0300

    modificated timer, added: babel, js-core

--- js/modules/timer.js
+++ js/modules/timer.js
@@ -1,64 +1,94 @@
 function timer(id, deadline) {
-   
 
-   function getTimeRemaing(endtime) {
-     let days, hours, minutes, seconds;
-     const t = Date.parse(endtime) - Date.parse(new Date());
 
-     if (t <= 0) {
-       days = 0;
-       hours = 0;
-       minutes = 0;
-       seconds = 0;
-     } else {
-       seconds = Math.floor((t / 1000) % 60),
-         minutes = Math.floor((t / 1000 / 60) % 60),
-         hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-         days = Math.floor(t / (1000 * 60 * 60 * 24));
-     }
+  function getTimeRemaing(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date());
 
-     return {
-       'total': t,
-       'days': days,
-       'hours': hours,
-       'minutes': minutes,
-       'seconds': seconds
-     };
-   }
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      seconds = Math.floor((t / 1000) % 60),
+        minutes = Math.floor((t / 1000 / 60) % 60),
+        hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+    }
 
-   function getZero(num) {
-     if (num >= 0 && num < 10) {
-       return `0${num}`;
-     } else {
-       return num
-     }
-   }
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
 
-   function setClock(selector, endtime) {
-     const timer = document.querySelector(selector),
-       days = timer.querySelector('#days'),
-       hours = timer.querySelector('#hours'),
-       minutes = timer.querySelector('#minutes'),
-       seconds = timer.querySelector('#seconds'),
-       timeInterval = setInterval(updateClock, 1000);
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num
+    }
+  }
 
-     updateClock();
+  const months = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
 
-     function updateClock() {
-       const t = getTimeRemaing(endtime);
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
 
-       days.innerHTML = getZero(t.days);
-       hours.innerHTML = getZero(t.hours);
-       minutes.innerHTML = getZero(t.minutes);
-       seconds.innerHTML = getZero(t.seconds);
+    updateClock();
 
-       if (t.total <= 0) {
-         clearInterval(timeInterval);
-       }
-     }
-   }
+    function updateClock() {
+      const timerBlock = document.querySelector('.promotion__timer')
+      let text = document.querySelector("#text");
+      const t = getTimeRemaing(endtime);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
-   setClock(id, deadline)
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+        timerBlock.style.display = 'none';
+        text.innerHTML = `Акция закончилась
+      <span>
+      ${new Date(endtime).getDate()} 
+      ${months[new Date(endtime).getMonth()]}
+      </span> в 00:00`; 
+      } else {
+        text.innerHTML = `Акция закончится
+      <span>
+      ${new Date(endtime).getDate()} 
+      ${months[new Date(endtime).getMonth()]}
+      </span> в 00:00`;
+      }
+    }
+ 
+
+  }
+
+  setClock(id, deadline)
 };
 
 export default timer;
--- js/script.js
+++ js/script.js
@@ -1,30 +1,36 @@
-    import tabs from './modules/tabs';
-    import timer from './modules/timer';
-    import slider from './modules/slider';
-    import modal from './modules/modal';
-    import forms from './modules/forms';
-    import cards from './modules/cards';
-    import calc from './modules/calc';
-    import { openModal  } from './modules/modal';
+ require('es6-promise').polyfill();
+ import 'nodelist-foreach-polyfill';
 
-    window.addEventListener('DOMContentLoaded', () => {
 
-        const modalTimerId = setTimeout(() => (openModal('.modal', modalTimerId)), 5000);
+import tabs from './modules/tabs';
+import timer from './modules/timer';
+import slider from './modules/slider';
+import modal from './modules/modal';
+import forms from './modules/forms';
+import cards from './modules/cards';
+import calc from './modules/calc';
+import {
+    openModal
+} from './modules/modal';
 
-        tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
-        timer('.timer', '2022-12-05');
-        slider({
-          container: '.offer__slider',
-          slide: '.offer__slide',
-          nextArrow: '.offer__slider-next',
-          prevArrow: '.offer__slider-prev',
-          totalCounter: '#total',
-          currentCounter: '#current',
-          wrapper: '.offer__slider-wrapper',
-          field: '.offer__slider-inner'
-        });
-        modal('[data-modal]', '.modal', modalTimerId);
-        forms('form', modalTimerId);
-        cards();
-        calc();
+window.addEventListener('DOMContentLoaded', () => {
+
+    const modalTimerId = setTimeout(() => (openModal('.modal', modalTimerId)), 5000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    timer('.timer', '2022-12-05');
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
     });
+    modal('[data-modal]', '.modal', modalTimerId);
+    forms('form', modalTimerId);
+    cards();
+    calc();
+});
